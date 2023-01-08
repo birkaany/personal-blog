76f4962c5dc3dba3841098166d132e2a74f58489
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2023-01-08 23:49:23 +0300

    style: configured tailwindcss for colors/font

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App bg-primary-gray font-mainFont">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "by-gray-100": "#fafafa",
+        "by-gray-400": "#5f5b58",
+        "by-black-800": "#1b1918",
+      },
+      fontFamily: {
+        mainFont: ["Lora", "serif"],
+      },
+    },
   },
   plugins: [],
 };
