27b1910e3885be741c311be20a7410ba61e4ec18
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2023-01-12 01:17:08 +0300

    fix: delete unused code

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { Link, Routes } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function HeroSection(props) {
+  const url = useParams();
   return (
     <section id="hero-section" className="container mx-auto border-b">
       {
-        (Routes.Link = "/post" && (
-          <div className="hero-text my-16 max-w-3xl">
-            <h1 className=" font-bold text-6xl font-mainFont">Things I have learned & experienced</h1>
-            <p className=" text-by-gray-400 font-mainFont text-xl py-8">In this blog, I write about the experiences and things I have learned on my journey to become a frontend developer.</p>
-          </div>
-        ))
+        <div className="hero-text my-16 max-w-3xl">
+          <h1 className=" font-bold text-6xl font-mainFont">Things I have learned & experienced</h1>
+          <p className=" text-by-gray-400 font-mainFont text-xl py-8">In this blog, I write about the experiences and things I have learned on my journey to become a frontend developer.</p>
+        </div>
       }
 
       <nav>
