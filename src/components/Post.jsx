14031a15a272352ed97d39d5263719a2758c4f54
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2023-01-13 12:06:08 +0300

    fix: request postdata for each post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
-export default function Post({ data }) {
+
+export default function Post() {
   let { postId } = useParams();
-  const currentPost = data.data?.find((item) => item.id == postId);
+  const [postData, setPostData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await fetch(`http://localhost:1337/api/posts/${postId}`);
+    const data = await res.json();
+    setPostData(data.data);
+  };
 
   return (
     <article>
       <div className="post container mx-auto">
-        <h1 className="text-by-black-800 text-3xl font-bold">{currentPost.attributes.title}</h1>
-        <time className="text-by-gray-400 font-bold uppercase text-sm tracking-wider">{currentPost.attributes.publishedAt}</time>
-        <ReactMarkdown>{currentPost.attributes.content}</ReactMarkdown>
+        <h1 className="text-by-black-800 text-3xl font-bold">{postData?.attributes?.title}</h1>
+        <time className="text-by-gray-400 font-bold uppercase text-sm tracking-wider">{postData?.attributes?.publishedAt}</time>
+        <ReactMarkdown>{postData?.attributes?.content}</ReactMarkdown>
       </div>
     </article>
   );
