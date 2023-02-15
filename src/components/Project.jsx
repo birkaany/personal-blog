4b984b799685652831479f4209fecac34e1769d7
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2023-02-16 01:53:51 +0300

    feat: add syntax highlighter w/ prismjs

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 import LayoutProvider from "../hooks/useLayout";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Icon from "./Icon";
 import { useParams } from "react-router-dom";
 import client from "../data/client";
+import Prism from "prismjs";
+import "prismjs/themes/prism-duotone.css";
 
 export default function Project() {
   const [projectData, setProjectData] = useState([]);
   const { projectId } = useParams();
+  const codeRef = useRef(null);
+
+  async function fetchData() {
+    const { data } = await client.items().type("project").equalsFilter("system.id", projectId).toPromise();
+    setProjectData(data.items);
+  }
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  async function fetchData() {
-    const { data } = await client.items().type("project").equalsFilter("system.id", projectId).toPromise();
-    setProjectData(data.items);
+  useEffect(() => {
+    if (codeRef.current) {
+      highlightCode();
+    }
+  }, [projectData]);
+
+  function highlightCode() {
+    const codeElements = codeRef.current.querySelectorAll("code");
+    codeElements.forEach((code) => {
+      const parent = code.parentElement;
+      const pre = document.createElement("pre");
+      parent.replaceChild(pre, code);
+      pre.appendChild(code);
+      code.className = "language-javascript";
+      Prism.highlightElement(code);
+    });
   }
 
   return (
@@ -23,7 +44,9 @@ export default function Project() {
       <article>
         <div className="post">
           <h1 className="text-by-black-800 text-3xl font-bold">{projectData[0]?.elements.project_title.value}</h1>
-          <div dangerouslySetInnerHTML={{ __html: projectData[0]?.elements.project_content.value }}></div>
+
+          <div dangerouslySetInnerHTML={{ __html: projectData[0]?.elements.project_content.value }} ref={codeRef}></div>
+
           <button className="project-button">
             Details
             <span>
