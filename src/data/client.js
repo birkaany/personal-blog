0eda4e380e75e9be0c69cd2eee0fbdebbed93fbd
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2023-02-17 02:03:37 +0300

    fix: env created

--- src/data/client.js
+++ src/data/client.js
@@ -1,6 +1,6 @@
 import { createDeliveryClient } from "@kontent-ai/delivery-sdk";
 const client = createDeliveryClient({
-  projectId: "53b3c05f-a8e3-00c8-6096-32effdcd9ae4",
+  projectId: process.env.PROJECT_ID,
 });
 
 export default client;
